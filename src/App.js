46e33fb4a84f3b40f73c7fa5202448e2e08522f2
rahulpathak1501 <rahul150101130015@gmail.com>
2023-12-12 18:45:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/albums")
+      .then((response) => response.json())
+      .then((json) => setAlbums(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="showAlbum">
+        <table>
+          <tbody>
+            <tr>
+              <th>Albums</th>
+            </tr>
+            {albums.map((album) => (
+              <tr key={album.id}>
+                <td>{album.title}</td>
+                <td>
+                  <button>Update</button>
+                </td>
+                <td>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
