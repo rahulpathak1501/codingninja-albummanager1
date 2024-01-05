a187dfba81826d07318416649dc9050a2a4fbdf6
rahulpathak1501 <rahul150101130015@gmail.com>
2024-01-05 11:38:48 +0530

    added a 404.html file in root directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-//import AddAlbum from "./Components/AddAlbum";
 import RoutComponents from "./Components/RouteComponents";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
